f69f06d4b17e6d74ad84a953daa177da773f79e4
SrRigby <gabriel25161323@gmail.com>
2021-06-18 15:40:32 -0300

    First commit

---
+++ src/skiller.js
@@ -0,0 +1,23 @@
+const skiller = (prefix) => {
+	return `
+╔════ ◤ 𝙍𝙄𝙂𝘽𝙔 𝘽𝙊𝙏✗✘◢
+║╔▸〘 𝙍𝙄𝙂𝘽𝙔 〙 
+║╠➥  *${prefix}alerta*
+║╠➥⚠️ para fazer um alerta
+║╠➥  *${prefix}divulgar*
+║╠➥⚠️ de aviso de videos
+║╠➥  *${prefix}bc*
+║╠➥⚠️ fazer tm para  todos os usuários
+║╠➥  *${prefix}setprefix*
+║╠➥⚠️ mudar o prefixo do bot
+║╠➥  *${prefix}hidetag*
+║╠➥⚠️ marcação invisível
+║╠➥*${prefix}clearall*
+║╠➥⚠️ limpar todos os chats
+║╠➥*${prefix}delete*
+║╠➥⚠️deletar mensagem
+║╚▸
+╚═══ ◤𝙍𝙄𝙂𝘽𝙔 𝘽𝙊𝙏✗✘◢ ` 
+}
+
+exports.skiller = skiller
